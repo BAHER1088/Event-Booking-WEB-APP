0d6a0d205511965fa81012ac44f4ea82f3f392e1
Bahy Adel <bahyadel515@gmail.com>
2025-05-14 16:53:11 +0300

    adding pagination

--- frontend/src/app/components/event-details/event-details.component.ts
+++ frontend/src/app/components/event-details/event-details.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { FooterComponent } from '../footer/footer.component';
+import { EventService } from '../../services/event.service';
 
 @Component({
   selector: 'app-event-details',
@@ -9,5 +10,22 @@ import { FooterComponent } from '../footer/footer.component';
   styleUrl: './event-details.component.scss'
 })
 export class EventDetailsComponent {
+  subscription: any;
+  id: string = '';
+  imgDomain: string = '';
+  event: any = {};
 
+  constructor(private _EventService :EventService) { }
+
+  loadevent() {
+    this.subscription = this._EventService.getOneEvent(this.id).subscribe((res) => {
+      this.event = res.data
+    })
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  ngOnDestroy(): void { this.subscription.unsubscribe(); }
 }
--- frontend/src/app/services/event.service.ts
+++ frontend/src/app/services/event.service.ts
@@ -17,11 +17,15 @@ export class EventService {
     this.eventImages =this._GlobalService.eventImages;
   }
 
-  getevents(limit: number = 8, page: number = 1): Observable<any> {
-    return this._HttpClient.get(`${this.hostName}${this.routeName}?limit=${limit}&page=${page}`)
+  getevents(limit: number = 8, page: number = 1,category?:string): Observable<any> {
+    if (category && category !== "") {
+    return this._HttpClient.get(`${this.hostName}${this.routeName}?category=${category}&limit=${limit}&page=${page}`);
+  } else {
+    return this._HttpClient.get(`${this.hostName}${this.routeName}?limit=${limit}&page=${page}`);
+  }
   }
 
-  // getOneevent(id: string): Observable<any> {
-  //   return this._HttpClient.get(`${this.hostName}${this.routeName}/${id}`)
-  // }
+  getOneEvent(id: string): Observable<any> {
+    return this._HttpClient.get(`${this.hostName}${this.routeName}/${id}`)
+  }
 }
